12310f6415327444eae205f4475e408d1f705ca7
Duane Grell <duanegrell@gmail.com>
2023-03-19 01:16:38 -0400

    passing tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,23 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL =  "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        occupation_list = []
+        occupations = json.loads(self.get_response_body())
+        for occupation in occupations:
+            occupation_list.append(occupation)
+        
+        return occupation_list
+    
+occupations = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+occupation_list = occupations.load_json()
+
+for occupation in occupation_list:
+    print (occupation)
+
+    
